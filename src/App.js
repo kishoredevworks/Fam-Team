7d25af6fd851f272b5ff2912f17d604415951599
k.kishore <kishore.junior.dev@gmail.com>
2024-10-18 18:22:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './Hcomponent';
 import PremiumNavbarHeader from './Header';
 import PremiumProjectsComponent from './Projects';
 import BrandSlider from './Slidebar';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Testimonials from './Testimonial';
 import TestimonialForm from './TestimonialForm';
 
@@ -16,16 +16,17 @@ function App() {
       <BrandSlider />
       <Testimonials />
       <TestimonialForm />
-      <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/projects" element={<PremiumProjectsComponent />} />
-  <Route path="/contact" element={<PremiumContactForm />} />
-  <Route path="/testimonials" element={<Testimonials />} />
-  <Route path="/testimonial-form" element={<TestimonialForm />} />
-  {/* Catch-all route for undefined paths */}
-  <Route path="*" element={<Home />} />
-</Routes>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<PremiumProjectsComponent />} />
+          <Route path="/contact" element={<PremiumContactForm />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/testimonial-form" element={<TestimonialForm />} />
+          {/* Catch-all route for undefined paths */}
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Router>
       <Footers />
     </div>
   );
